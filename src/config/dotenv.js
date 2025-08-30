2848bfc6ab818c6a010edbedfbdc0fcdc5920ab8
angel-iscoding <angel.iscoding@gmail.com>
2025-08-29 22:54:04 -0500

    Database errors hotfix

--- src/config/dotenv.js
+++ src/config/dotenv.js
@@ -13,4 +13,4 @@ export const DB_HOST = process.env.DB_HOST || "localhost";
 export const DB_USER = process.env.DB_USER || "root";
 export const DB_PASSWORD = process.env.DB_PASSWORD || "admin";
 export const DB_DATABASE = process.env.DB_DATABASE || "RoomieZ";
-export const DB_PORT = process.env.DB_PORT || 30061;
+export const DB_PORT = process.env.DB_PORT || 3061;
