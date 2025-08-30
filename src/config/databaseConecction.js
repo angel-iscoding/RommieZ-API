b4676bdce5a8f329233ab9f9ad833f08a400b043
angel-iscoding <angel.iscoding@gmail.com>
2025-08-29 22:24:46 -0500

    Added default env values

--- src/config/databaseConecction.js
+++ src/config/databaseConecction.js
@@ -2,11 +2,11 @@ import mysql2 from 'mysql2/promise';
 import { DB_DATABASE, DB_HOST, DB_PASSWORD, DB_PORT, DB_USER } from './dotenv.js';
 
 const pool = mysql2.createPool({
-    host: DB_HOST,
-    user: DB_USER,
-    password: DB_PASSWORD,
-    database: DB_DATABASE,
-    port: DB_PORT,
+    host: DB_HOST || "localhost",
+    user: DB_USER || "root",
+    password: DB_PASSWORD || "164980",
+    database: DB_DATABASE || "test",
+    port: DB_PORT || 3306,
 });
 
 export default pool;
